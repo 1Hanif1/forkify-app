2c6010f2820c8d5cfad37c434bb2bf098a2f467c
Hanif Barbhuiya <74130996+1Hanif1@users.noreply.github.com>
2022-09-15 10:31:56 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SECONDS = 10;
 export const MODAL_CLOSE_SECONDS = 2.5;
 export const RESULTS_PER_PAGE = 10;
-export const API_KEY = undefined; // Place your own API key here :)
+export const API_KEY = "8d488d17-fae0-474f-a48f-b7eab7d8c578"; // Place your own API key here :)
